2493e35457b80632ff4062cf58791da4eafb098b
ApplePie <applepie@ApplePies-Mac.local>
2019-01-23 23:57:10 +0300

    Спроектирован экран добавления дня рождения

---
+++ Tracker/Tracker/Source/ViewController/AddBirthdayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddBirthdayViewController.swift
+//  Tracker
+//
+//  Created by Волнухин Виктор on 23/01/2019.
+//
+
+import UIKit
+
+class AddBirthdayViewController: UIViewController {
+    
+    // MARK: - controls
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var birthdatePicker: UIDatePicker!
+    
+
+    // MARK: - lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
