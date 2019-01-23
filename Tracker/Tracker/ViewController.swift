87c3b319a997de5c27a6d9bcf12012a6b769d74e
Волнухин Виктор <volnukhin.v@rfc1918.private.ip.localhost>
2019-01-23 18:44:11 +0300

    Создание проекта

---
+++ Tracker/Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Волнухин Виктор on 23/01/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
